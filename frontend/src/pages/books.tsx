553ce49419037627900233d9086da7c403d3fcda
Amir438-Abdullah <anaabdullah77777@gmail.com>
2021-05-04 19:28:18 +0600

    added typescript

--- frontend/src/pages/books.tsx
+++ frontend/src/pages/books.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { PrismaClient } from "@prisma/client";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+
 import Masonry from "react-masonry-css";
 
-export default function Books({ data }) {
-  console.log(data);
+interface DataTypes {
+  img_link: string;
+  name: string;
+  author: string;
+  id: number;
+}
+
+interface BooksProps {
+  data: Array<DataTypes>;
+}
+
+export const Books: React.FC<BooksProps> = ({ data }) => {
+  console.log;
 
   const breakpointColumnsObj = {
     default: 5,
@@ -13,8 +26,11 @@ export default function Books({ data }) {
   };
 
   // Convert array to JSX items
-  const allBooks = data.map(({ img_link, name, author }) => (
-    <div className="bg-gray-700 py-1 px-2 flex flex-col flex-nowrap rounded m-2">
+  const allBooks = data.map(({ img_link, name, author, id }) => (
+    <div
+      key={id}
+      className="bg-gray-700 py-1 px-2 flex flex-col flex-nowrap rounded m-2"
+    >
       <img src={img_link} className="  block rounded" alt={name} />
       <div className="mt-2">
         <h2 className="text-gray-200  text-lg font-bold">{name}</h2>
@@ -32,9 +48,11 @@ export default function Books({ data }) {
       {allBooks}
     </Masonry>
   );
-}
+};
 
-export async function getServerSideProps() {
+export default Books;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const prisma = new PrismaClient();
   const books = await prisma.allbooks.findMany();
 
@@ -43,4 +61,4 @@ export async function getServerSideProps() {
       data: books,
     },
   };
-}
+};
